475a0001ecf43d574eb04bcd0ce4f54bbcb89344
adarshguptaji007-maker <adarshguptaji007@gmail.com>
2026-06-04 13:42:15 +0530

    loop added

---
+++ loop.py
@@ -0,0 +1,27 @@
+# print Adarsh 5 times
+
+print('Adarsh')
+print('Adarsh')
+print('Adarsh')
+print('Adarsh')
+print('Adarsh')
+
+# 1.Initializion 
+# 2 . Condition 
+#3. Increement/decreement 
+
+for i in range(0,11):
+
+  print("Adarsh")
+
+# while(i>45):
+#     print("hhh")
+    
+#     i+=1 
+
+    #while
+#   while True:
+#      print("😎 Code chalu hai... Ctrl + C dabao warna main nahi rukunga!")
+
+for i in iter(int, 1):
+    print("🌶️ Namkeen Loop Chalu Hai!")
